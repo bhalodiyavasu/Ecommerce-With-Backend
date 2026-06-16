685aea3e378d157926d57a0976f468b212298e0f
bhalodiyavasu <vasubhalodiya49@gmail.com>
2026-06-16 17:15:30 +0530

    feat: implement user login functionality with JWT authentication and protect middleware

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
@@ -36,4 +37,48 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+      return res.status(400).json({ status: "FAILURE", message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ status: "FAILURE", message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({status: "FAILURE", message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      sameSite: "strict",
+    });
+
+    res.status(200).json({
+      status: "SUCCESS",
+      message: "Login successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    })
+  } catch (error) {
+    res.status(500).json({ status: "FAILURE", message: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,7 +1,27 @@
+// const express = require('express');
+// const { registerUser, loginUser } = require('../controllers/authController');
+
+// const router = express.Router();
+// router.post("/register", registerUser)
+// router.post("/login", loginUser)
+
+// module.exports = router;
+
 const express = require('express');
-const { registerUser } = require('../controllers/authController');
+const { registerUser, loginUser } = require('../controllers/authController');
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
-router.post("/register", registerUser)
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+// Protected route — test mate
+router.get("/me", protect, (req, res) => {
+  res.status(200).json({
+    status: "SUCCESS",
+    user: req.user,
+  });
+});
 
 module.exports = router;
